70e198f8f50ff634a399e74d2a447d9dede77e55
onur <guresonur@gmail.com>
2021-05-13 02:55:25 +0300

    v0.1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -43,7 +43,7 @@ data() {
     return {
       client_id: '821e61dd7fda4f70a421c9fdba582349',
       scopes: 'user-read-playback-state user-library-read user-read-recently-played playlist-read-private',
-      redirect_uri: 'https://spotifyimsi-ehgy1dgdo-guresonur.vercel.app/',
+      redirect_uri: 'http://localhost:8080/',
       me: null,
       current_track_name: null,
       payload: null,
@@ -197,6 +197,7 @@ data() {
 
 .header {
     align-items: center;
+    background-color: #121212;
 }
 
 
